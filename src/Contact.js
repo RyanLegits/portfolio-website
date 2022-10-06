1c708946ea10bd1a1f9b2adc5dd4f51d67245478
RyanLegits <ryan.legits@gmail.com>
2022-10-06 16:52:37 -0600

    Connect contact form to email using nodemailer

--- src/Contact.js
+++ src/Contact.js
@@ -1,13 +1,46 @@
 import { useState } from 'react';
 
 const Contact = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [mailerState, setMailerState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
-  const handleSubmit = (e) => {
+  function handleStateChange(e) {
+    setMailerState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  const submitEmail = async (e) => {
     e.preventDefault();
-    const formInfo = { name, email, message };
+    console.log({ mailerState });
+    const response = await fetch('http://localhost:3001/send', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ mailerState }),
+    })
+      .then((res) => res.json())
+      .then(async (res) => {
+        const resData = await res;
+        console.log(resData);
+        if (resData.status === 'success') {
+          alert('Message Sent');
+        } else if (resData.status === 'fail') {
+          alert('Message failed to send');
+        }
+      })
+      .then(() => {
+        setMailerState({
+          email: '',
+          name: '',
+          message: '',
+        });
+      });
   };
 
   return (
@@ -15,27 +48,30 @@ const Contact = () => {
       <h2>Contact</h2>
       <div className="contact-sec-cont">
         <h5>Contact Me</h5>
-        <form className="contact-form" onSubmit={handleSubmit}>
+        <form className="contact-form" onSubmit={submitEmail}>
           <input
             type="text"
             placeholder="Your Name"
             required
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            onChange={handleStateChange}
+            name="name"
+            value={mailerState.name}
           />
           <input
             type="text"
             placeholder="Your Email"
             required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={handleStateChange}
+            name="email"
+            value={mailerState.email}
           />
           <textarea
             type="text"
             placeholder="Message"
             required
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            onChange={handleStateChange}
+            name="message"
+            value={mailerState.message}
           />
           <button id="submit-btn" className="button">
             Submit
