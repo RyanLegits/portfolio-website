8f212d187dfa89ce4acb9a85a143bb0017e7f03d
RyanLegits <ryan.legits@gmail.com>
2022-10-05 23:35:45 -0600

    Create ‘Contact’ form
    Create 'Contact' form fields and test submit action works

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,47 @@
+import { useState } from 'react';
+
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formInfo = { name, email, message };
+  };
+
   return (
     <div className="contact">
       <h2>Contact</h2>
-      <div className="contact-cont"></div>
+      <div className="contact-sec-cont">
+        <h5>Contact Me</h5>
+        <form className="contact-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Your Name"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Your Email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <textarea
+            type="text"
+            placeholder="Message"
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button id="submit-btn" className="button">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
